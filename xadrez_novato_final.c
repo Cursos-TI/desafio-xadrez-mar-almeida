2c5c9a704d3eddec290e0ed9652dc7a28e4fb746
Marco Almeida <200988745+mar-almeida@users.noreply.github.com>
2025-05-09 01:51:49 +0000

    xadrez_novato_final_Completo

--- xadrez_novato_final.c
+++ xadrez_novato_final.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
 
+int main (){
+
+    int torre;
+    int bispo = 0; 
+    int rainha = 0; 
+    char UserChoice;
+   
+    do
+    {
+        printf("***XADREZ NOVATO***\n");
+        printf("A. Torre\n");
+        printf("B. Bispo\n");
+        printf("C. Rainha\n");
+        printf("Escolha: ");
+        scanf(" %c", &UserChoice);
+            if (UserChoice == 'A' || UserChoice == 'a')
+            {
+                printf("Você escolheu a Torre!\n");
+                for (torre = 0; torre < 5; torre++)
+                {
+                    printf("Cima\n");
+                }
+                
+            } else if (UserChoice == 'B' || UserChoice == 'b')
+            {
+                printf("Você escolheu o Bispo!\n");
+                do
+                {
+                    printf("Cima + Direita\n");
+                    bispo++;
+
+                } while (bispo < 5);
+                
+            } else if (UserChoice == 'C' || UserChoice == 'c')
+            {
+                printf("Você escolheu a Rainha!\n");
+                while (rainha < 8)
+                {
+                    printf("Esquerda\n");
+                    rainha++;
+                }
+
+            } else {
+                printf("Opção Inválida!\n");
+            }
+
+    } while ((UserChoice != 'A' && UserChoice != 'a') && 
+            (UserChoice != 'B' && UserChoice != 'b') && 
+            (UserChoice != 'C' && UserChoice != 'c'));
+
+    
+    return 0;
+
+}
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -27,6 +82,3 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
-}
